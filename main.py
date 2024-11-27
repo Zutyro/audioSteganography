8c84d2be2ca97662e9f917f433759988891cfbbd
Tomáš Turecký <t_turecky@utb.cz>
2024-11-27 21:26:05 +0100

    Copied functions from @alwinjoseph7 Steganography Phase coding repo

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import lsb
+import phase
 
 def main():
-    # lsb.encode_aud_data("watch-this-spoken_C_minor", "tohle je testova veta")
-    lsb.decode_aud_data("watch-this-spoken_C_minornew")
-
-
+    # lsb.encode_aud_data("watch-this-spoken_C_minor", "tohle je testova veta 2")
+    # lsb.decode_aud_data("watch-this-spoken_C_minornew")
+    # phase.encode("sound81.wav","Tohle je taky testova veta 3")
+    print(phase.decode("encoded.wav"))
 
 
 
